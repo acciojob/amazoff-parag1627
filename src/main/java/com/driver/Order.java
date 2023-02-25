8af222a2d2b9a76db377285c8707b25af67a0b34
parag1627 <paragpatil973047@gmail.com>
2023-02-25 15:47:51 +0530

    hope

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,11 +9,42 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id = id;
+        int hour = Integer.valueOf(deliveryTime.substring(0, 2));
+        int min = Integer.valueOf(deliveryTime.substring(3));
+        this.deliveryTime = hour * 60 + min;
     }
 
     public String getId() {
         return id;
     }
 
-    public int getDeliveryTime() {return deliveryTime;}
+    public int getDeliveryTime() {
+        return deliveryTime;}
 }
+
+
+
+
+
+
+
+//package com.driver;
+//
+//public class Order {
+//
+//    private String id;
+//    private int deliveryTime;
+//
+//    public Order(String id, String deliveryTime) {
+//
+//        // The deliveryTime has to converted from string to int and then stored in the attribute
+//        //deliveryTime  = HH*60 + MM
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public int getDeliveryTime() {return deliveryTime;}
+//}/
